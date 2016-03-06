fe837bf546bed02b76fa2f9f7d102e8ac392169d
Jaroslav Siroic <jaroslav.siroic@gmail.com>
2016-03-06 12:31:40 +0200

    checkers looks nice but doesn work

---
+++ mvc5-unit-of-work-example-master/UnitOfWorkExample.Domain/Services/RoleService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnitOfWorkExample.Domain.Entities;
+using UnitOfWorkExample.Domain.Repositories;
+
+namespace UnitOfWorkExample.Domain.Services
+{
+
+    public interface IRoleService
+    {
+        IList<Role> GetAll();
+        Role GetById(int id);
+        void Create(Role role);
+        void Update(Role role);
+        void Delete(int id);
+    }
+    public class RoleService : IRoleService
+    {
+        private IRepository<Role> roleRepository;
+
+        public RoleService(IRepository<Role> roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+
+        public IList<Role> GetAll()
+        {
+            return roleRepository.GetAll().ToList();
+        }
+
+        public Role GetById(int id)
+        {
+            return roleRepository.GetById(id);
+        }
+
+        public void Create(Role role)
+        {
+            roleRepository.Update(role);
+        }
+
+        public void Update(Role role)
+        {
+            roleRepository.Update(role);
+        }
+
+        public void Delete(int id)
+        {
+            roleRepository.Delete(id);
+        }
+    }
+}
--- mvc5-unit-of-work-example-master/UnitOfWorkExample.Web/Global.asax.cs
+++ mvc5-unit-of-work-example-master/UnitOfWorkExample.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace UnitOfWorkExample.Web
 {
@@ -13,6 +14,33 @@ namespace UnitOfWorkExample.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            // Users module covers it:
+
+            var authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            var roleCokie = Request.Cookies["COOKIE!!!!"];
+
+            if (authCookie != null)
+            {
+                try
+                {
+                    var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                    if (authTicket != null)
+                    {
+                        var identity = new FormsIdentity(authTicket);
+                        var principal = roleCokie == null ? new RolePrincipal("RoleProvider", identity) : new RolePrincipal(identity, roleCokie.Value);
+                        Context.User = principal;
+                    }
+                }
+                catch
+                {
+                    Session.Clear();
+                    FormsAuthentication.SignOut();
+                }
+            }
         }
     }
 }
